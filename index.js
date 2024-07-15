2657b50052eda0260946c5dd357dd9934aed8946
Suraj Thapa <surajthapart@gmail.com>
2024-07-15 20:45:21 +0545

    i give up for today lets do it tomorrow

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send("Successfully done baby");
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
