35e553faaf70491f53d3863549aa65df31dfa59d
Suraj Thapa <surajthapart@gmail.com>
2024-07-16 13:50:18 +0545

    completed this first project using express and node js

--- index.js
+++ index.js
@@ -22,11 +22,33 @@ app.get("/", function (req, res) {
   
 });
 
+
+app.get("/files/:fileName", function (req, res) {
+  fs.readFile(`./files/${req.params.fileName}`,"utf-8",function(err, filedata){
+    res.render('./show', {filename: req.params.fileName, filedata: filedata})
+  })
+  
+});
+
+app.get("/edit/:fileName", function (req, res) {
+  fs.readFile(`./files/${req.params.fileName}`,"utf-8",function(err){
+    res.render('./edit', {filename: req.params.fileName})
+  })
+  
+});
+
 app.post("/create", function (req, res) {
   fs.writeFile(`./files/${req.body.task.split(" ").join("")}.txt`, req.body.desc, function(err){
     res.redirect("/")
   })
 
 });
+
+app.post("/edit", function (req, res) {
+    fs.rename(`./files/${req.body.prev}`, `./files/${req.body.update}`, function(err){
+      res.redirect("/")
+    })
+    
+  })
 
 app.listen(3000);
