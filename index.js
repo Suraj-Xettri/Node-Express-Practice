fc7decae692198cec697cff62c72051ca269a91e
Suraj Thapa <surajthapart@gmail.com>
2024-07-16 09:33:12 +0545

    installed many dependencies

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-import express from "express";
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/", function (req, res) {
   res.send("It's Working");
