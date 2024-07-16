7a426d65ee57dfd4c69b10831cdac96b2b1833ad
Suraj Thapa <surajthapart@gmail.com>
2024-07-16 09:06:10 +0545

    All set up done

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send("Successfully done baby");
+app.get("/", function (req, res) {
+  res.send("It's Working");
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(3000);
